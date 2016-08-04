60a8044fb674bef05fe8849ed4157eb982084aeb
Rich Linnell <richlinnell@gmail.com>
2016-08-04 20:22:54 +0100

    Initial Commit

---
+++ duck-tape.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import  { bootstrap } from '@angular/platform-browser-dynamic';
+
+
+@Component({
+	selector:'duck-tape',
+	template:'<h1> Frog = {{frog}} </h1>'
+})
+
+class DuckTapeComponent{
+	frog : number;
+
+	constructor(){
+		this.frog = 100;
+		setInterval(()=>this.updo(), 500);
+	}
+	private updo(): void {
+		this.frog++;
+	}
+
+}
+bootstrap(DuckTapeComponent);
